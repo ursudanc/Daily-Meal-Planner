5847b50e160ae5c8056a90dcbeab1c8fb5f2ddf6
Fabian Virgil Pacuraru <Pacuraru.Li.Fabian@student.utcluj.ro>
2022-03-18 15:44:56 +0200

    login database structure

---
+++ DMP/Models/Login.cs
@@ -0,0 +1,9 @@
+namespace Login.Models
+{
+    public class Login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool admin { get; set; }
+    }
+}
